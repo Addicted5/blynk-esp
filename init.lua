e15930c794b48ed165ba041f3e617b36df560ee6
Addicted5 <ashutoshprusty29@gmail.com>
2016-11-14 19:35:13 +0530

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,45 +1,13 @@
 print ( "Starting blynk example" )
 
-token = ""
-wifi_password = ""
-wifi_access_point = ""
-
--- Connect to my wifi
-function connecting(wifi_ssid, wifi_password)
-    wifi.setmode(wifi.STATION)
-    wifi.sta.config(wifi_ssid, wifi_password)
-
-    tmr.alarm(0, 500, 1, function()
-        if wifi.sta.getip()==nil then
-          print("Connecting to AP...")
-        else
-          tmr.stop(1)
-          tmr.stop(0)
-          print("Connected as: " .. wifi.sta.getip())
-          b = blynk.new ( token, 4, set_callbacks ):connect()
-        end
-    end)
-
-    tmr.alarm(1, 15000, 0, function()
-        if wifi.sta.getip()==nil then
-            tmr.stop(0)
-            print("Failed to connect to \"" .. wifi_ssid .. "\"")
-        end
-    end)
-end
+token = "YourSecretToken"
+wifi_password = "12345678"
+wifi_access_point = "BlynkIt"
 
 
 -- Load the Blynk library
 dofile ( 'blynk.lua' )
-if wifi_access_point == "" and wifi_password == "" then
-   print ("=============\nPlease edit init.lua and set:\n\tWiFi Access Point\n\tPassword\n\tBlynk token")
-   print ("=============")
-else
-   if wifi.sta.getip()==nil then
-      print ( "connecting to AP..." )
-      connecting(wifi_access_point, wifi_password)
-   else
-      print ("already connected")
-      b = blynk.new ( token, 4, set_callbacks ):connect()
-   end
-end
+-- Load application part
+dofile ( 'application.lua' )
+-- connect to WiFi 
+dofile ( 'wifi.lua' )
